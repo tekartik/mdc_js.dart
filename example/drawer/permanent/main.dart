af9b3f1650540b48ffa777a5edc96735ced040d4
Alexandre Roux Tekartik <alex@tekartik.com>
2018-08-17 11:24:32 +0200

    build: dart2

--- example/drawer/permanent/main.dart
+++ example/drawer/permanent/main.dart
@@ -1,12 +1,9 @@
 // Copyright (c) 2017, alex. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'dart:html';
-
 import 'package:tekartik_common_utils/async_utils.dart';
 import 'package:tekartik_mdc_js/loader.dart';
 import 'package:tekartik_app_utils/material_asset/loader.dart';
-import 'package:tekartik_mdc_js/mdc_js.dart';
 
 main() async {
   await waitAll([
